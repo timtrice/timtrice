9d59d8679f8389ccfa139bf739fa34f8e8e253bb
timtrice <tim.trice@gmail.com>
2016-02-02 22:08:04 -0600

    still buggy with edits on line #160. Does upload, updates posts dataset. Made some major changes to meta tags - must be prefixed with 'wp_'. Will do a README for this function

--- wp-publish.R
+++ wp-publish.R
@@ -1,161 +1,204 @@
-#' wpPublish
-#'
-#' See https://codex.wordpress.org/XML-RPC_MetaWeblog_API for additional details
-#' https://codex.wordpress.org/XML-RPC_WordPress_API/Categories_%26_Tags
-#' Sample header of a markdown file:
-#' 
-#' ---
-#' title: Drafts and Super Bowl Quarterbacks
-#' description: The Quick Red Fox Jumps Over The Lazy Brown Dog. The Quick Red Fox Jumps Over The Lazy Brown Dog. 
-#' post_type: post
-#' categories: Sports, Football
-#' mt_keywords: football, super bowl
-#' wp_slug: drafted-super-bowl-quarterbacks
-#' wp_author_display_name: timtrice
-#' ---
-#' 
-#' The "---" strings are not required but good for readability.
-#' 
-#' @param ext character file extension of the markdown
-#' @param edit boolean TRUE if editing a post
-#' @param publish boolean TRUE if the post should be published
-#' @param post_type character c("post", "page")
-#' @param wp_author_display_name character author's display name. See WordPress Profile
-#'
-#' @todo add editing functionality
-#' @todo connect to dataframe to store post info OR connect to MySQL
-#' @todo description doesn't push through. 
-#' @todo allow some meta to push through as defaults rather than header
-#' 
 
 
-
-wpPublish <- function(ext = "*.Rmd", edit = FALSE, publish = FALSE, 
-                      post_type = "post", wp_author_display_name = "timtrice") {
+wpPublish <- function(ext = "\\.[R]?md", # Extension of markdown files
+                      publish = FALSE,  # TRUE if want WP to publish post; 
+                      # otherwise, uploaded as draft
+                      post_type = "post", # post or page
+                      author = "timtrice", 
+                      shortcode = FALSE) { # Use [shortcode] instead of <pre>
     
-    getTag <- function(infile, c, tag) {
+    getArticle <- function(infile) {
+        con <- file(infile)
+        open(con)
+        article <- readLines(con)
+        close(con)
+        return(article)
+    }
+    
+    getTag <- function(infile = NULL, article = NULL, tag = NULL) {
         
-        extract_tag <- grep(tag, contents, value = TRUE)
+        if(any(is.null(infile), is.null(article), is.null(tag))) 
+            stop("One or more parameter is NULL.")
+        
+        extract_tag <- grep(tag, article, value = TRUE)
         
         if(length(extract_tag) == 0) {
-            warning(sprintf("No %s tag present in %s", tag, infile))
             return (NULL)
         }
         
         tag <- sub(paste(tag, "[ :]+(.)", sep = ""), "\\1", extract_tag)
         
         return(tag)
+    } # End getTag
+    
+    getMeta <- function(infile = NULL, article = NULL) {
+        
+        if(any(is.null(infile), is.null(article))) 
+            stop("One or more parameter is NULL.")
+        
+        meta <- list("wp_title" = getTag(infile, article, "wp_title"), 
+                     "wp_description" = getTag(infile, article, 
+                                               "wp_description"), 
+                     "wp_post_type" = getTag(infile, article, 
+                                             "wp_post_type"), 
+                     "wp_categories" = getTag(infile, article, 
+                                              "wp_categories"), 
+                     "wp_mt_keywords" = getTag(infile, article, 
+                                               "wp_mt_keywords"), 
+                     "wp_slug" = getTag(infile, article, "wp_slug"), 
+                     "wp_author" = getTag(infile, article, "wp_author"))
+        
+        return(meta)
     }
     
-    publishPost <- function(file, rmd, meta) {
+    doPublish <- function(infile) { 
         
-        require(RWordPress)
+        article <- getArticle(infile)
         
-        source("../wordpress.R", local = TRUE)
-        
-        options(WordpressLogin = c(timtrice = wppw),
-                WordpressURL = "http://timtrice.co/xmlrpc.php")
-        
-        require(knitr)
-        require(markdown)
-        opts_knit$set(upload.fun = function(file){
-            library(RWordPress) 
-            uploadFile(file)$url
-        })
-        opts_knit$set(unnamed.chunk.label = meta$wp_slug)
-        opts_chunk$set(fig.cap = "center")
-        
-        post_id <- knit2wp(file, title = trimws(meta$title), 
-                           categories = trimws(meta$categories[[1]]), 
-                           description = trimws(meta$description), 
-                           mt_keywords = trimws(meta$mt_keywords[[1]]), 
-                           post_type = trimws(post_type), 
-                           wp_author_display_name = trimws(wp_author_display_name), 
-                           action = "newPost", publish = publish, 
-                           shortcode = FALSE)
-        
-        return (post_id)
-        
-    }
-    
-    current_wd <- getwd()
-    
-    setwd("./")
-    
-    for (infile in list.files(".", pattern = ext, full.names = TRUE, 
-                              recursive = TRUE)) {
-        
-        # Get the file directory and file name w/ extension        
-        file_dir <- strsplit(infile, "/")
-        file_name <- file_dir[[1]][[length(file_dir[[1]])]]
-        
-        # Open file and get content, close file
-        con <- file(infile)
-        open(con)
-        contents <- readLines(con)
-        close(con)
-        
-        # If draft == TRUE, go to next post
-        draft <- getTag(infile, contents, "draft")
-        if(draft == TRUE || is.null(draft)) next
-        
-        title <- getTag(infile, contents, "title")
-        description <- getTag(infile, contents, "description")
-        post_type <- getTag(infile, contents, "post_type")        
-        categories <- getTag(infile, contents, "categories")
-        categories <- strsplit(categories, ",")
-        mt_keywords <- getTag(infile, contents, "mt_keywords")
-        mt_keywords <- strsplit(mt_keywords, ",")
-        wp_slug <- getTag(infile, contents, "wp_slug")
-        wp_author_display_name <- getTag(infile, contents, 
-                                         "wp_author_display_name")
-        
-        meta <- list("title" = title, 
-                     "description" = description, 
-                     "post_type" = post_type, 
-                     "categories" = categories, 
-                     "mt_keywords" = mt_keywords, 
-                     "wp_slug" = wp_slug, 
-                     "wp_author_display_name" = wp_author_display_name)
-        
-        # Check if post already exists
-        if(!exists("posts")) posts <- read.csv("./data/posts.csv")
-        
-        require("dplyr")
-        
-        post_id <- posts %>% filter(file_name == file_name) %>% 
-            select(post_id)
-        
-        # Post already exists but edit is FALSE; can't work.
-        if(nrow(post_id) == 1 & edit == FALSE) {
-            warning(paste(file_name, 
-                          "already exists with this file name;", 
-                          "Edit is FALSE. Ignoring."), 
-                    sep = " ")
-            next
-        } else if(nrow(post_id) > 1) {
-            # Uh oh
-            warning(paste("Too many posts with", file_name, sep = " "))
-            next
-        } else if(nrow(post_id) == 0) {
-            # New post
-            post_id <- publishPost(file = infile, rmd = infile, meta = meta)
-            if(post_id == 401) {
-                warning("Not published. User does not have permission.")
-            } else if(post_id == 404) {
-                warning("Not published. Invalid post format.")
-            } else { 
-                message("Page/Post Published")
-                newPosts <- data.frame(post_id = post_id[1], 
-                                       file_name = file_name)
-                posts <- rbind(posts, newPosts)
-                write.csv(posts, "./data/posts.csv", row.names = FALSE)
-                message("Posts DF updated!")
-            }
-        } else if(nrow(post_id) == 1 & edit == TRUE) {
-            # Edit post
+        # If article is localDraft or wp_draft tag not set, skip
+        localDraft <- getTag(infile, article, "wp_draft")
+        if(is.null(localDraft)) { 
+            message(sprintf("%s not posted. No draft tag present.", infile))
+            return(NULL)
+        } else if(as.logical(localDraft)) { 
+            message(sprintf("%s not posted. wp_draft == TRUE", infile))
+            return(NULL)
         }
         
-    }
+        # At this point localDraft == TRUE
+        
+        # import posts
+        if(!exists("posts")) posts <- read.csv("./data/posts.csv", 
+                                               stringsAsFactors = FALSE)
+        require("dplyr")
+        post <- posts %>% 
+            filter(file_name == infile) %>% 
+            select(post_id, mtime)
+        
+        # Does post already exist?
+        if(nrow(post) == 0 || nrow(post) == 1) {
+            
+            # Get post meta
+            meta <- getMeta(infile, article)
+            
+            # Clean and trim wp_categories
+            if(length(meta$wp_categories) > 0) {
+                meta$wp_categories <- unlist(strsplit(meta$wp_categories, 
+                                                      ","))
+                meta$wp_categories <- unlist(lapply(meta$wp_categories, 
+                                                    FUN = trimws))
+            }
+            
+            # Clean and trim wp_mt_keywords
+            if(length(meta$wp_mt_keywords) > 0) {
+                meta$wp_mt_keywords <- unlist(strsplit(meta$wp_mt_keywords, 
+                                                       ","))
+                meta$wp_mt_keywords <- unlist(lapply(meta$wp_mt_keywords, 
+                                                     FUN = trimws))
+            }
+            
+            # Need to check if categories and tags exist, 
+            # Not sure if WP requires this; check it out
+            
+            # If wp_slug doesn't exist, use filename
+            if(length(meta$wp_slug) == 0) 
+                meta$wp_slug = sub(paste0("(.)", ext), "\\1", basename(infile))
+            
+            # If wp_author doesn't exist, use author param
+            if(length(meta$wp_author) == 0) meta$wp_author = author
+            
+            # At this point we're ready to start uploading
+            require(RWordPress)
+            
+            source("../wordpress.R", local = TRUE)
+            
+            options(WordpressLogin = c(timtrice = wppw),
+                    WordpressURL = "http://timtrice.co/xmlrpc.php")
+            
+            require(knitr)
+            require(markdown)
+            opts_knit$set(unnamed.chunk.label = meta$wp_slug)
+            opts_chunk$set(fig.cap = "center")
+            
+            opts_knit$set(upload.fun = function(file){
+                uploadFile(file)$url
+            })
+            
+            if(nrow(post) == 0) {
+                
+                # Publishing a new post
+                return_code <- knit2wp(infile, 
+                                       title = meta$wp_title, 
+                                       categories = meta$wp_categories,  
+                                       description = meta$wp_description, 
+                                       mt_keywords = meta$wp_mt_keywords, 
+                                       post_type = meta$wp_post_type, 
+                                       wp_author_display_name = meta$wp_author, 
+                                       action = "newPost", 
+                                       publish = publish, 
+                                       shortcode = shortcode)
+                
+                if(return_code == 401) {
+                    warning(paste("Error 401: User does not have permission", 
+                                  "to write/edit this post.", sep = " "))
+                    return(NULL)
+                } else if(return_code == 404) {
+                    warning("Error 404: Invalid post format specified")
+                    return(NULL)
+                }
+                
+                
+                message(sprintf("%s uploaded", basename(infile)))
+                newPosts <- data.frame(post_id = return_code[1], 
+                                       file_name = infile, 
+                                       mtime = file.info(infile)$mtime)
+                posts <- rbind(posts, newPosts)                
+                
+            } else {
+                
+                # Editing an existing post
+                return_code <- knit2wp(infile, 
+                                       title = meta$wp_title, 
+                                       categories = meta$wp_categories,  
+                                       description = meta$wp_description, 
+                                       mt_keywords = meta$wp_mt_keywords, 
+                                       post_type = meta$wp_post_type, 
+                                       wp_author_display_name = meta$wp_author, 
+                                       action = "editPost", 
+                                       postid = post_id, 
+                                       publish = publish, 
+                                       shortcode = shortcode)
+                
+                if(return_code == 401) {
+                    warning(paste("Error 401: User does not have permission", 
+                                  "to write/edit this post."))
+                    return(NULL)
+                } else if(return_code == 404) {
+                    warning(paste("Error 404: Invalid post format specified", 
+                                  "or postid does not exist.", sep = " "))
+                    return(NULL)
+                }
+                
+                message(sprintf("%s updated", basename(infile)))
+                posts[posts$post_id == post_id]$mtime <- "test"
+                
+            }
+            
+            write.csv(posts, "./data/posts.csv", row.names = FALSE)
+            rm(posts)
+            message(sprintf("%s uploaded successfully!", basename(infile)))
+            
+        } else {
+            # Problem
+            stop("Too many posts!")
+        }
+        
+    } # End doPublish
+    
+    infile <- list.files(".", pattern = ext, full.names = TRUE, 
+                         recursive = TRUE)
+    
+    code <- lapply(infile, FUN = doPublish)
     
 }
+
