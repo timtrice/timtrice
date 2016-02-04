537d2f2510299a409cf0215d1a1eee2b610efab1
Tim Trice <tim.trice@gmail.com>
2016-02-04 14:09:54 -0600

    Update predict-pressure.R

--- R/weather/HURDAT/predict-pressure.R
+++ R/weather/HURDAT/predict-pressure.R
@@ -21,9 +21,6 @@ co <- coef(fs)
 predict(fit)
 
 # Predict barometric pressure based on wind
-wind <- 180
-1.027e+03 + (-6.602e-01) * wind
-
 pna <- dt[is.na(Pressure) & !is.na(Wind), .(ID, Wind, Pressure)]
 pna <- pna[, Pressure := round(fs$coefficients[1,1] + (fs$coefficients[2,1]) * Wind)]
 pna <- pna[, Wind := NULL]
@@ -31,3 +28,5 @@ pna <- pna[, Wind := NULL]
 # Write.csv
 write.csv(pna, file = "hurdat.predict.pressure.csv", quote = FALSE, 
     row.names = FALSE)
+
+# And we're done
