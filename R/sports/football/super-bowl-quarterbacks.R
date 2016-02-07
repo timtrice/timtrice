57ed727989672ae061e3888695aebb14bc7d5072
timtrice <tim.trice@gmail.com>
2016-02-07 14:37:53 -0600

    renamed QB to Name

--- R/sports/football/super-bowl-quarterbacks.R
+++ R/sports/football/super-bowl-quarterbacks.R
@@ -4,8 +4,8 @@ library(readr)
 library(tidyr)
 
 #' Execute before starting to assure we have the latest data
-source("draft-qb.R")
-source("super-bowl-standings.R")
+source("./R/sports/football/draft-qb.R")
+source("./R/sports/football/super-bowl-standings.R")
 
 #' Setting
 data_dir <- "./data/sports/football"
@@ -29,33 +29,33 @@ sb_standings <- separate(sb_standings, StartingQB, c(paste0("QB", 1:4)),
                          remove = FALSE, sep = ",", fill = "right")
 
 #' Gather all QB Columns back to one. D1 will be removed later
-sb_standings <- sb_standings %>% gather(D1, QB, QB1:QB4)
+sb_standings <- sb_standings %>% gather(D1, Name, QB1:QB4)
 
 #' We have some NA's under value we can get rid of. 
-sb_standings <- sb_standings[complete.cases(sb_standings$QB),]
+sb_standings <- sb_standings[complete.cases(sb_standings$Name),]
 
-#' I've spot checked the new QB column versus StartingQB and, happy with the 
+#' I've spot checked the new Name column versus StartingQB and, happy with the 
 #' results I'll drop StartingQB and D1
-sb_standings <- select(sb_standings, Tm, QB)
+sb_standings <- select(sb_standings, Tm, Name)
 
 #' Now I'll add a win/loss column
 patt <- "([A-Za-z ']+).*?([0-9]{1})-([0-9]{1})."
 sb_standings <- sb_standings %>% 
-    group_by(QB) %>% 
-    mutate(Wins = as.integer(sub(patt, "\\2", QB)), 
-           Losses = as.integer(sub(patt, "\\3", QB)))
+    group_by(Name) %>% 
+    mutate(Wins = as.integer(sub(patt, "\\2", Name)), 
+           Losses = as.integer(sub(patt, "\\3", Name)))
 
 #' And I'll add a winning percentage (WP) then order by Name
 sb_standings <- sb_standings %>% 
-    group_by(QB, Tm) %>% 
+    group_by(Name, Tm) %>% 
     mutate(WP = as.numeric(sprintf("%0.3f", Wins/sum(Wins, Losses)))) %>% 
-    arrange(QB)
+    arrange(Name)
 
-#' I'll clean the W-L record from the QB varaible
-sb_standings$QB <- gsub(patt, "\\1", sb_standings$QB)
+#' I'll clean the W-L record from the Name varaible
+sb_standings$Name <- gsub(patt, "\\1", sb_standings$Name)
 
 #' Clean whitespace
-sb_standings$QB <- trimws(sb_standings$QB)
+sb_standings$Name <- trimws(sb_standings$Name)
 
 #' Lastly, save the dataset
 write.csv(sb_standings, paste(data_dir, csv_filename, sep = "/"), quote = FALSE, 
