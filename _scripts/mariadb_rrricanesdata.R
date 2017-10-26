ac3225debffce0c9fc9e9ebb0a34b13ec8edcf18
Tim Trice <tim.trice@gmail.com>
2017-10-26 08:23:12 -0500

    Update conn params, do not write tidy dataframes

--- _scripts/mariadb_rrricanesdata.R
+++ _scripts/mariadb_rrricanesdata.R
@@ -8,24 +8,23 @@ host <- Sys.getenv("andromeda.host")
 user <- Sys.getenv("andromeda.user")
 password <- Sys.getenv("andromeda.password")
 
-con <- dbConnect(MariaDB(), host = host, user = user,
-                 password = password)
+con <- dbConnect(MariaDB(), host = host, user = user, password = password)
 
 dbSendQuery(con, "CREATE DATABASE rrricanesdata")
 
 dbSendQuery(con, "USE rrricanesdata")
 
-dbWriteTable(con, "adv", as.data.frame(adv), overwrite = TRUE)
+# dbWriteTable(con, "adv", as.data.frame(adv), overwrite = TRUE)
 dbWriteTable(con, "discus", as.data.frame(discus), overwrite = TRUE)
-dbWriteTable(con, "fcst", as.data.frame(fcst), overwrite = TRUE)
-dbWriteTable(con, "fcst_wr", as.data.frame(fcst_wr), overwrite = TRUE)
+# dbWriteTable(con, "fcst", as.data.frame(fcst), overwrite = TRUE)
+# dbWriteTable(con, "fcst_wr", as.data.frame(fcst_wr), overwrite = TRUE)
 dbWriteTable(con, "fstadv", as.data.frame(fstadv), overwrite = TRUE)
 dbWriteTable(con, "posest", as.data.frame(posest), overwrite = TRUE)
 dbWriteTable(con, "prblty", as.data.frame(prblty), overwrite = TRUE)
 dbWriteTable(con, "public", as.data.frame(public), overwrite = TRUE)
-dbWriteTable(con, "storms", as.data.frame(storms), overwrite = TRUE)
+# dbWriteTable(con, "storms", as.data.frame(storms), overwrite = TRUE)
 dbWriteTable(con, "update", as.data.frame(update), overwrite = TRUE)
 dbWriteTable(con, "wndprb", as.data.frame(wndprb), overwrite = TRUE)
-dbWriteTable(con, "wr", as.data.frame(wr), overwrite = TRUE)
+# dbWriteTable(con, "wr", as.data.frame(wr), overwrite = TRUE)
 
 dbDisconnect(con)
